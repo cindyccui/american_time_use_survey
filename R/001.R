b029725fe685306b73a6699cec6712f958d9509f
m-dz <mat.dziedzic+git@gmail.com>
2017-03-10 08:33:46 +0000

    Fix regexes, change few names, melt and fwrite

--- R/001.R
+++ R/001.R
@@ -3,12 +3,16 @@ library(data.table)
 library(magrittr)
 library(Hmisc)
 library(stringr)
+library(ggplot2)
 
 # https://www.kaggle.com/bls/american-time-use-survey
 # https://www.reddit.com/r/dataisbeautiful/comments/5xodqv/how_we_spend_our_days_oc/
 
-dt_cols <- fread(file.path('data_source', 'atussum.csv'), nrows = 0)
 
+
+# Check col names ---------------------------------------------------------
+
+dt_cols <- fread(file.path('data_source', 'atussum.csv'), nrows = 0)
 id_cols <- c(
   'tucaseid',
   'teage',
@@ -18,18 +22,30 @@ id_cols <- c(
   'tudiaryday', # Day of the week of diary day (day of the week about which the respondent was interviewed), 1 - Sunday
   'tuyear'      # Year of diary day (year of day about which respondent was interviewed) 
 )
-sel_cols <- names(dt_cols) %>% str_detect('^t[0-9]+') %>% { names(dt_cols)[.] }
+cat_cols <- names(dt_cols) %>% str_detect('^t[0-4]+') %>% { names(dt_cols)[.] }
 
-dt <- fread(file.path('data_source', 'atussum.csv'), select = c(id_cols, sel_cols))
 
-# Confirm no duplicated tucaseid in 'atussum.csv'
-# identical(dt[, tucaseid], dt[, unique(tucaseid)])
+
+# Read data ---------------------------------------------------------------
+
+dt <- fread(file.path('data_source', 'atussum.csv'), select = c(id_cols, cat_cols)) %>%
+  setnames(
+    c('tucaseid', 'teage', 'tesex', 'trdpftpt', 'trernwa', 'tudiaryday', 'tuyear'),
+    c('ID','Age','Gender','Empl._status','Weekly_salary','Day_of_week','Year')
+  )
+
+# Confirm no duplicated IDs in 'atussum.csv'
+# identical(dt[, ID], dt[, unique(ID)])
+
+
+
+# Process activities ------------------------------------------------------
 
 # Activity codes here https://www.bls.gov/tus/lexiconnoex0315.pdf
 lookup <- tibble::tribble(
   ~key, ~category,
   '^t0101.*',          'Sleep',                                  # Includes Sleeplessness, t010102
-  '^t01[0,9][^1].*',   'Personal care',                          # All except t010101/02/99, includes t019999, so t01[0,9][^1]any number
+  '^t01(?!01).*',      'Personal care',                          # All except starting with t0101, all t01... included
   '^t02.*',            'Household chores',
   '^t03.*',            'Caring for household members',
   '^t04.*',            'Caring for non-hh members',
@@ -40,8 +56,7 @@ lookup <- tibble::tribble(
   '^t09.*',            'Household services',
   '^t10.*',            'Government services',
   '^t11.*',            'Eating and drinking',
-  # '^t12.*',           'Socializing, Relaxing, Leisure',
-  '^t12(?!030[3-8])', 'Socializing, Relaxing, Leisure',
+  '^t12(?!030[3-8])',  'Socializing, Relaxing, Leisure',
   '^t12030[3-8]',      'TV / Radio / Computer',
   '^t13.*',            'Sports, Exercise, Recreation',
   '^t14.*',            'Religious & Spiritual activities',
@@ -50,25 +65,74 @@ lookup <- tibble::tribble(
   '^t18.*',            'Travelling'
 ) %>% data.table()
 
-lookup
+# Check regexes
+lapply(lookup %>% t() %>% data.table, function(x) {
+  grep(x[1], names(dt), value = TRUE, perl = TRUE)
+})
+
+# Set args for grep
+perl_grep <- function(x, y) grep(x, y, value = TRUE, perl = TRUE)
 
 for(i in seq_len(nrow(lookup))) {
-  dt[, (lookup[i, category]) := sum(.SD), .SDcols = names(dt) %like% lookup[i, key], by = tucaseid]
+  # dt[, .SD, .SDcols = perl_grep(lookup[i, key], names(dt))] %>% print()
+  dt[, (lookup[i, category]) := rowSums(.SD), .SDcols = perl_grep(lookup[i, key], names(dt))]
+  dt[, (perl_grep(lookup[i, key], names(dt))) := NULL]
 }
 
-dt[, age_bins := cut(teage, breaks = c(-Inf, seq(0, 100, by = 5), +Inf))]
-dt[, age_bins]
 
-dt_cooked <- dt[, mget(c(id_cols, lookup[, category]))]
 
-fwrite(dt_cooked, file.path('data', 'atussum_cooked.csv'))
+# Rescale to 24h ----------------------------------------------------------
 
-dt_cooked[, total := sum(.SD), .SDcols = lookup[, category]]
+dt[, Total := rowSums(.SD), .SDcols = lookup[, category]]
+ggplot(dt, aes(x = Total)) + geom_histogram(bins = 100) + theme_classic()
 
-# Melt
-dt_cooked <- melt(dt_cooked, id.vars = c(1:7))
+# Rescale
+dt[, prop.table(table(Total == 1440))]  # 24h
+dt[Total != 1440, (lookup[, category]) := .SD * 1440/Total, .SDcols = lookup[, category]]
 
-# Remove media leisure from Leisure
-dt_cooked
+dt[, Total_rescaled := rowSums(.SD), .SDcols = lookup[, category]]
+ggplot(dt, aes(x = Total_rescaled)) + geom_histogram(bins = 100) + theme_classic()
 
-dt_cooked[tucaseid == 20030100013280, sum(value)]
+ggplot(dt[Work >= 60, ], aes(x = Work)) + geom_histogram(binwidth = 15) + theme_minimal()
+
+dt[, Working_day := as.numeric(Work >= 60)]
+mdmisc::reorder_columns(dt, 'Working_day after Day_of_week')
+
+
+
+# Melt --------------------------------------------------------------------
+
+dt_melted <- melt(
+  dt[, !c('Total', 'Total_rescaled')], id.vars = c(1:8),
+  variable.name = 'Category', value.name = 'Minutes',
+  variable.factor = FALSE)
+
+
+
+# By Age ------------------------------------------------------------------
+
+# dt_melted[Age == 15, .(Avg_mins = mean(Minutes)), by = c('Age', 'Category')] %>%
+#   .[, Category := str_trim(Category)] %>% setorder(Age, Category) %>% .[, sum(Avg_mins)] %>% print()
+
+#    Age                              Category     Avg_mins
+# 1:  15          Caring for household members   6.40444287
+# 2:  15             Caring for non-hh members   6.69300096
+# 3:  15                    Consumer purchases  19.59461602
+# 4:  15                   Eating and drinking  54.52740070
+# 5:  15                             Education 181.78777092
+
+# From https://www.kaggle.com/halhen/d/bls/american-time-use-survey/what-we-give-up-for-work
+# 1   15          Caring for household members   6.34214391
+# 2   15             Caring for non-hh members   6.64757709
+# 3   15                    Consumer purchases  19.34459129
+# 4   15                   Eating and drinking  54.06265296
+# 5   15                             Education 180.80518845
+
+
+
+# Save --------------------------------------------------------------------
+
+fwrite(dt,        file.path('data', 'atussum_cooked.csv'))
+fwrite(dt_melted, file.path('data', 'atussum_cooked_melted.csv'))
+
+
